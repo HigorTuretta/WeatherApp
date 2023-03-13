a1a95b758d5a1002d16be6cb081c963038d29a8b
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2023-03-13 18:31:19 -0300

    mainJS path update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { updateSVG } from "./utils/updateweatherImage";
+import { updateSVG } from "./app/utils/updateweatherImage";
 
 //Variaveis e seleção de objetos
 const apiKey = "22e966f689b50d30020f27ef9d990ccd";
